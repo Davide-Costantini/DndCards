c9dbd5653497011a47b9110ca7e053991abc64d7
Davide-Costantini <davide0998@gmail.com>
2024-01-18 15:35:16 +0100

    Predisposition of the form to create the cars
    Added components to input the data and preview the card
    Added resource manager

---
+++ DndCards/DndCards/Models/CardItem.cs
@@ -0,0 +1,15 @@
+﻿namespace DndCards.Models
+{
+    public class CardItem
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public bool RequiresAttunement { get; set; } = false;
+        public string Type { get; set; } = string.Empty;
+        public string Characteristics { get; set; } = string.Empty;
+        public string Footer { get; set; } = string.Empty;
+        public string ImageUri { get; set; } = string.Empty;
+        public int ImageWidth { get; set; } = 300;
+        public int ImageHeight { get; set; } = 500;
+    }
+}
